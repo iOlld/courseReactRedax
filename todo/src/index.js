e9befc069e7eeaf6e42df7a4ffe12a6ebeb6e291
Zheka-ded <11vitamin11@rambler.ru>
2020-05-07 06:44:07 +0300

    the application is broken down into components and a style (important) is added to the to-do list

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,33 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// const el = React.createElement('h1', null, 'Hello world!!!!');
-// const el = <h1>Hello World</h1>;
-// console.log(el);
-
-const TodoList = () => {
-
-    const items = ['Learn React', 'Build Awesome APP'];
-    return (
-        <ul>
-            <li>{items[0]}</li>
-            <li>{items[1]}</li>
-        </ul>
-    );
-};
-
-const AppHeader = () => {
-    return <h1>My Todo List</h1>;
-};
-
-const SearchPanel = () => {
-    const searchText = 'Type here to search';
-    const searchStyle = {
-        fontSize: '20px',
-    }
-    return <input placeholder={searchText}
-                    style={searchStyle} />;
-};
+import AppHeader from './components/AppHeader/AppHeader';
+import TodoList from './components/TodoList/TodoList';
+import SearchPanel from './components/SearchPanel/SearchPanel';
 
 const App = () => {
 
